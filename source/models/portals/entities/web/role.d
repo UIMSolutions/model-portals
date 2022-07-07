2c3150e5152a7178229769f3d1f4f99395e05cae
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-07-07 17:43:24 +0200

    Updates

--- source/models/portals/entities/web/role.d
+++ source/models/portals/entities/web/role.d
@@ -12,27 +12,27 @@ class DPortalWebRoleEntity : DOOPEntity {
 
     this
       .addValues([
-        CreatedOnBehalfByAttribute, // Shows who created the record on behalf of another user."]),
-        ModifiedOnBehalfByAttribute, // Shows who last updated the record on behalf of another user."]),
+        CreatedOnBehalfByAttribute, // Shows who created the record on behalf of another user.,
+        ModifiedOnBehalfByAttribute, // Shows who last updated the record on behalf of another user.,
+        OwnerIdAttribute, 
         StatusCodeAttribute, // Reason for the status of the Web Role
-        WebRoleIdAttribute, // Unique identifier for entity instances"]),
-        WebSiteIdAttribute, // Unique identifier for Website associated with Web Role."]),
+        WebRoleIdAttribute, // Unique identifier for entity instances,
+        WebSiteIdAttribute, // Unique identifier for Website associated with Web Role.,
       ])
       .addValues([
-        "overriddenCreatedOn": TimestampAttribute, // Date and time that the record was migrated."]),
-        "importSequenceNumber": NumberAttribute, //Sequence number of the import that created this record."]),
-        "ownerId": UUIDAttribute, // Owner Id"]),
-        "ownerIdType": StringAttribute, // The type of owner, either User or Team."]),
-        "owningBusinessUnitId": BusinessUnitIdAttribute, // Unique identifier for the business unit that owns the record"]),
-        "owningUserId": UserIdAttribute, // Unique identifier of the user that owns the activity."]),
-        "owningTeamId": TeamIdAttribute, // , // Unique identifier for the team that owns the record."]),
-        "timeZoneRuleVersionNumber": NumberAttribute, //For internal use only."]),
-        "utcConversionTimeZoneCode": StringAttribute, // Time zone code that was in use when the record was created."]),
-        "stateCode": StringAttribute, // Status of the Web Role"]),
+        "overriddenCreatedOn": TimestampAttribute, // Date and time that the record was migrated.,
+        "importSequenceNumber": NumberAttribute, //Sequence number of the import that created this record.,
+        "ownerIdType": StringAttribute, // The type of owner, either User or Team.,
+        "owningBusinessUnitId": BusinessUnitIdAttribute, // Unique identifier for the business unit that owns the record,
+        "owningUserId": UserIdAttribute, // Unique identifier of the user that owns the activity.,
+        "owningTeamId": TeamIdAttribute, // , // Unique identifier for the team that owns the record.,
+        "timeZoneRuleVersionNumber": NumberAttribute, //For internal use only.,
+        "utcConversionTimeZoneCode": StringAttribute, // Time zone code that was in use when the record was created.,
+        "stateCode": StringAttribute, // Status of the Web Role,
         "stateCode_display": StringAttribute, // 
         "authenticatedUsersRole": StringAttribute, // 
         "anonymousUsersRole": StringAttribute, // 
-        "key": StringAttribute, // An alternate key that is not intended to be localized to allow retrieval of a specific Web Role in workflows or code."]),
+        "key": StringAttribute, // An alternate key that is not intended to be localized to allow retrieval of a specific Web Role in workflows or code.,
       ])
       .registerPath("portal_webroles");
   } 
@@ -53,13 +53,6 @@ version(test_model_portals) {
   unittest {
     assert(PortalWebRoleEntity);
   
-  auto entity = PortalWebRoleEntity;
-  // auto repository = OOPFileRepository("./tests");
-/* /*  repository.create("entities", entity.entityClasses, entity.toJson);
-
-  auto json = repository.findOne("entities", entity.entityClasses, ["id":entity.id.toString]);
-  assert(json != Json(null), entity.id.toString~" not found");
-
-  repository.cleanupConnections; */ 
+    auto entity = PortalWebRoleEntity;
   }
 }
