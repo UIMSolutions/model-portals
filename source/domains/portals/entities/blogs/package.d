f6856a3cae1aa4b56a24ac569a366d6835975a39
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-11-14 06:38:47 +0100

    Rename root folder to domains

---
+++ source/domains/portals/entities/blogs/package.d
@@ -0,0 +1,8 @@
+module models.portals.entities.blogs;
+
+@safe:
+import models.portals;
+
+public import models.portals.entities.blogs.blog;
+
+public import models.portals.entities.blogs.post;
---
+++ source/domains/portals/entities/forums/package.d
@@ -0,0 +1,10 @@
+module models.portals.entities.forums;
+
+@safe:
+import models.portals;
+
+public import models.portals.entities.forums.forum;
+
+public import models.portals.entities.forums.post;
+public import models.portals.entities.forums.thread;
+public import models.portals.entities.forums.thread_type;
