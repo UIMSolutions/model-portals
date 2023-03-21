a687e986a933de3321afcabc448a9b9a42ebf6ec
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2023-03-21 08:37:57 +0100

    rename

---
+++ source/models /portals/entities/package.d
@@ -0,0 +1,18 @@
+module models.portals.entities;
+
+@safe:
+import models.portals;
+
+// Packages
+public import models.portals.entities.ads;
+public import models.portals.entities.badges;
+public import models.portals.entities.blogs;
+public import models.portals.entities.cases;
+public import models.portals.entities.forums;
+public import models.portals.entities.ideas;
+public import models.portals.entities.polls;
+public import models.portals.entities.web;
+
+// Modules
+public import models.portals.entities.account;
+public import models.portals.entities.external_identity;
