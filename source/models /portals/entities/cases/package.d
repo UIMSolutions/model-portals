66b38311dbf84abf99ee0dc6476d25034c1fea6f
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2023-03-23 07:50:07 +0100

    Errorcorretion with Github upload

--- source/models /portals/entities/cases/package.d
+++
@@ -1,8 +0,0 @@
-module models.portals.entities.cases;
-
-@safe:
-import models.portals;
-
-public import models.portals.entities.cases.case_;
-
-public import models.portals.entities.cases.deflection;
