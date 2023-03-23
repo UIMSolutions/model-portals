cf259227d2951b92834d5f4abdec8d9d80ee8b92
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2023-03-23 07:50:39 +0100

    Errorcorretion with Github upload

---
+++ source/modelx/portals/attributes/uuids/entities/ads/ad.d
@@ -0,0 +1,24 @@
+module models.portals.attributes.uuids.entities.ads.ad;
+
+@safe:
+import models.portals;
+
+class DAdIdAttribute : DEntityIdAttribute {
+  mixin(AttributeThis!("AdIdAttribute"));
+
+  override void initialize(DConfigurationValue configSettings = null) {
+    super.initialize(configSettings);
+
+    this
+      .name("adId")
+      .registerPath("adId");
+  }  
+}
+mixin(AttributeCalls!("AdIdAttribute"));
+
+version(test_model_portals) {
+  unittest {
+    testAttribute(new DAdIdAttribute);
+    testAttribute(AdIdAttribute);
+  }
+}
---
+++ source/modelx/portals/attributes/uuids/entities/package.d
@@ -0,0 +1,6 @@
+module models.portals.attributes.uuids.entities;
+
+public import models.portals.attributes.uuids.entities.ads;
+public import models.portals.attributes.uuids.entities.polls;
+public import models.portals.attributes.uuids.entities.posts;
+public import models.portals.attributes.uuids.entities.web;
