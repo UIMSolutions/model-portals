72edb8964d2a1e3f4e7a0baf7425bb78d2550125
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2023-03-23 07:49:46 +0100

    Errorcorretion with Github upload

--- source/models /portals/attributes/uuids/entities/package.d
+++
@@ -1,6 +0,0 @@
-module models.portals.attributes.uuids.entities;
-
-public import models.portals.attributes.uuids.entities.ads;
-public import models.portals.attributes.uuids.entities.polls;
-public import models.portals.attributes.uuids.entities.posts;
-public import models.portals.attributes.uuids.entities.web;
