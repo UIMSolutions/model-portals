35329f92a04e823631e54b3779b6d0d0ee02cb1f
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-03-26 10:06:22 +0200

    Updates because github crash

--- source/modelx/portals/attributes/uuids/entities/posts/package.d
+++
@@ -1,5 +0,0 @@
-module models.portals.attributes.uuids.entities.posts;
-
-public import models.portals.attributes.uuids.entities.posts.post;
-public import models.portals.attributes.uuids.entities.posts.first;
-public import models.portals.attributes.uuids.entities.posts.last;
